aa01d03e648d45717b7c5e5aebf84d714e1402ab
Gabriel <gabrielgapseu@gmail.com>
2024-04-22 11:48:57 -0300

    Refactor: alteration on authRoutes http method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ import AuthController from "../controllers/AuthController.js";
 
 const authRoutes = Router()
 
-authRoutes.get("/sign-up", AuthController.signUp)
-authRoutes.get("/sign-in", AuthController.signIn)
+authRoutes.post("/sign-up", AuthController.signUp)
+authRoutes.post("/sign-in", AuthController.signIn)
 
 export default authRoutes;
