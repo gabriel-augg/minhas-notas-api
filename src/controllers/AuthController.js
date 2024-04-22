39c4ee68903ad547617b8488ca8351469c080a5e
Gabriel <gabrielgapseu@gmail.com>
2024-04-22 11:42:31 -0300

    Feat: signin function

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -3,6 +3,40 @@ import bcrypt from "bcrypt"
 import createToken from "../helpers/create-token.js";
 
 export default class AuthController{
+
+    static async signIn(req, res){
+        const { email, password } = req.body
+
+        if( !email || !password ){
+            return res.status(400).json({
+                message: "Ocorreu um erro inesperado!"
+            })
+        }
+
+        const user = await User.findOne({where: {email: email}})
+
+        if(!user){
+            return res.status(401).json({
+                message: "Senha ou email incorreto!"
+            })
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword){
+            return res.status(401).json({
+                message: "Senha ou email incorreto!"
+            })
+        }
+
+        try {
+            await createToken(user, req, res)
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({message: "Ocorreu um erro no servidor!"})
+        }
+    }
+
     static async signUp(req, res){
         const { name, email, password, confirmpassword } = req.body
 
@@ -12,9 +46,9 @@ export default class AuthController{
             })
         }
 
-        const checkIfUserExist = await User.findOne({where: {email: email}})
+        const checkIfUserExists = await User.findOne({where: {email: email}})
 
-        if(checkIfUserExist){
+        if(checkIfUserExists){
             return res.status(409).json({
                 message: "O email já está em uso!"
             })
@@ -44,4 +78,5 @@ export default class AuthController{
         }
 
     }
+
 }
