2d3ad9c3c83876b0fad8880c46632adfbbb05b89
CristianDevk <lucristian.oliveira2007@gamil.com>
2024-04-24 18:26:19 -0300

    Feat: adding routes: /delete/:id, /update/:id

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -3,10 +3,16 @@ import { Router } from "express";
 import NoteController from "../controllers/NoteController.js";
 import verifyToken from "../helpers/verify-token.js";
 
-
-const noteRoutes = Router()
+const noteRoutes = Router();
 
 noteRoutes.post("/create", verifyToken, NoteController.create)
 noteRoutes.get("/get-notes", verifyToken, NoteController.getAllUserNotes)
+noteRoutes.put("/update/:id", verifyToken, NoteController.update)
+noteRoutes.delete("/delete/:id", verifyToken, NoteController.delete)
 
 export default noteRoutes;
+//faculdade ou trabalho
+
+//seria no mesmo esquema das rotas, então teria um tagRoutes ou nah?
+
+// ah tá
