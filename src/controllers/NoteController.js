dbd228aa6357069225011f3ab24644e487f5ff31
CristianDevk <lucristian.oliveira2007@gamil.com>
2024-04-24 18:24:56 -0300

    Feat: create routes: Update and Delete

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -1,4 +1,3 @@
-import { Op } from "sequelize";
 import getToken from "../helpers/get-token.js";
 import getUserByToken from "../helpers/get-user-by-token.js";
 import Note from "../models/Note.js";
@@ -47,6 +46,58 @@ export default class NoteController {
             res.status(500).json({ 
                 message: "Ocorreu um erro inesperado com o servidor, por favor, tente novamente mais tarde" 
             })
+        }
+    }
+
+    static async update(req, res) {
+        const { id } = req.params;
+        const { title = null, description = null, tag = null, pinned = null } = req.body;
+    
+        try {
+            const note = await Note.findByPk(id);
+            if(!note){
+                res.status(404).json({
+                    message: "A nota não foi encontrada"
+                });
+            } 
+
+            note.title = title;
+            note.description = description;
+            note.tag = tag;
+            note.pinned = pinned;
+
+            await note.save();
+            console.log(note);
+
+            res.status(200).json({note})
+        } catch (error) {
+            res.status(500).json({ 
+                message: "Ocorreu um erro inesperado com o servidor, por favor, tente novamente mais tarde" 
+            });
+        }
+    }
+
+    static async delete (req, res) {
+        const { id } = req.params;
+        try {
+            const note = await Note.findByPk(id);
+            if(!note){
+                res.status(404).json({
+                    message: "A nota não foi encontrada"
+                });
+            } 
+            
+            await note.destroy(
+                {
+                    where:{id}
+                });
+            res.status(200).json({
+                message: "Nota deletada com sucesso"
+            });
+        } catch (error) {
+            res.status(500).json({ 
+                message: "Ocorreu um erro inesperado com o servidor, por favor, tente novamente mais tarde" 
+            });
         }
     }
 }
