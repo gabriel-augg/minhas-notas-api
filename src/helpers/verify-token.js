57ebe843ce034401831080f62261fb2fd137b9eb
Gabriel <gabrielgapseu@gmail.com>
2024-05-11 19:55:43 -0300

    Fix: bugfix

--- src/helpers/verify-token.js
+++ src/helpers/verify-token.js
@@ -20,8 +20,7 @@ const verifyToken = (req, res, next) => {
         jwt.verify(token, "31a31204-b3a4-4195-b252-d48278285630");
         return next();
     } catch (error) {
-        console.log(error);
-        return res.status(401).json({
+        res.status(401).json({
             message: "Token inválido!",
         });
     }
